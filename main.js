7a741620813ed9341f7e5321c7f39984ffab5ed5
Wladimir Miller <wladim.miller@gmail.com>
2025-12-29 11:40:33 +0100

    Erstellung QUIZ App

---
+++ main.js
@@ -0,0 +1,60 @@
+//Definition der Fragen und Antworten
+const quizQuestions = [
+  {
+    id: 1,
+    question: "Was ist die Hauptstadt von Deutschland?",
+    answers: [
+      { id: "a", text: "Berlin", isCorrect: true },
+      { id: "b", text: "München", isCorrect: false },
+      { id: "c", text: "Frankfurt", isCorrect: false },
+      { id: "d", text: "Hamburg", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "Welche Einwohnerzahl hat Deutschland?",
+    answers: [
+      { id: "a", text: "83 Millionen", isCorrect: true },
+      { id: "b", text: "70 Millionen", isCorrect: false },
+      { id: "c", text: "90 Millionen", isCorrect: false },
+      { id: "d", text: "100 Millionen", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "Welcher Fluss fließt durch Berlin?",
+    answers: [
+      { id: "a", text: "Spree", isCorrect: true },
+      { id: "b", text: "Elbe", isCorrect: false },
+      { id: "c", text: "Rhein", isCorrect: false },
+      { id: "d", text: "Donau", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Welches dieser Autos stammt aus Deutschland?",
+    answers: [
+      { id: "a", text: "BMW", isCorrect: true },
+      { id: "b", text: "Ford", isCorrect: false },
+      { id: "c", text: "Toyota", isCorrect: false },
+      { id: "d", text: "Volkswagen", isCorrect: false },
+    ],
+  },
+];
+
+// Initialisierung
+let currentQuestionIndex = [
+  {
+    id: 0,
+    question: "",
+    answers: [{ id: "", text: "", isCorrect: false }],
+  },
+];
+
+// Frage Render Funktion
+
+// Next Logik
+
+//Frage beantworten Funktion
+
+// Lösung anzeigen Funktion
